ae5e0f7c453c2a0916756eae49e87ef9f38fd9dc
Edson Silva <edson_nasilva@hotmail.com>
2019-06-09 10:58:26 -0300

    Revert "Correção."
    This reverts commit 902d30f4b3cf4c14c26c69f4ee36f85f8f870971.

--- Decision.API/Controllers/CadastroController.cs
+++ Decision.API/Controllers/CadastroController.cs
@@ -16,7 +16,7 @@ namespace Decision.Api.Controllers
     {
         // subscriptionKey = "0123456789abcdef0123456789ABCDEF"
         private const string subscriptionKey = "1df9a911fd2844d5a75db1026761bcd2";
-
+                
         private const string remoteImageUrl =
            "https://amazonasatual.com.br/wp-content/uploads/2018/08/CNH-falsa-Manaus.jpeg";
 
@@ -38,61 +38,53 @@ namespace Decision.Api.Controllers
 
         public async Task<IHttpActionResult> Post()
         {
+            HttpRequestMessage request = this.Request;
+
+            ComputerVisionClient computerVision = new ComputerVisionClient(
+                new ApiKeyServiceClientCredentials(subscriptionKey),
+                new System.Net.Http.DelegatingHandler[] { });
+
+            // Specify the Azure region
+            computerVision.Endpoint = "https://eastus2.api.cognitive.microsoft.com";
+
+            RecognizeTextInStreamHeaders analysis = null;
+            using (Stream imageStream = await request.Content.ReadAsStreamAsync())
+            {
+                analysis = await computerVision.RecognizeTextInStreamAsync(imageStream, TextRecognitionMode.Printed);
+            }
+
+            string operation = analysis.OperationLocation.Split('/').GetValue(analysis.OperationLocation.Split('/').Length - 1).ToString();
+
+            Thread.Sleep(5000);
+
+            TextOperationResult result = await computerVision.GetTextOperationResultAsync(operation);
+
             Cadastro cadastro = new Cadastro();
 
-            try
+            cadastro.Nome = result.RecognitionResult.Lines[5].Text;
+            cadastro.RG = result.RecognitionResult.Lines[7].Text;
+            cadastro.CPF = result.RecognitionResult.Lines[11].Text;
+
             {
-                //HttpRequestMessage request = this.Request;
+                DateTime resultado;
 
-                ComputerVisionClient computerVision = new ComputerVisionClient(
-                    new ApiKeyServiceClientCredentials(subscriptionKey),
-                    new System.Net.Http.DelegatingHandler[] { });
-
-                // Specify the Azure region
-                computerVision.Endpoint = "https://eastus2.api.cognitive.microsoft.com";
-
-                RecognizeTextHeaders analysis = null;
-                analysis = await computerVision.RecognizeTextAsync("https://amazonasatual.com.br/wp-content/uploads/2018/08/CNH-falsa-Manaus.jpeg", TextRecognitionMode.Printed);
-                //using (Stream imageStream = await request.Content.ReadAsStreamAsync())
-                //{
-                //    analysis = await computerVision.RecognizeTextInStreamAsync(imageStream, TextRecognitionMode.Printed);
-                //}
-
-                string operation = analysis.OperationLocation.Split('/').GetValue(analysis.OperationLocation.Split('/').Length - 1).ToString();
-
-                Thread.Sleep(5000);
-
-                TextOperationResult result = await computerVision.GetTextOperationResultAsync(operation);
-
-                cadastro.Nome = result.RecognitionResult.Lines[5].Text;
-                cadastro.RG = result.RecognitionResult.Lines[7].Text;
-                cadastro.CPF = result.RecognitionResult.Lines[11].Text;
-
+                if (DateTime.TryParse(result.RecognitionResult.Lines[12].Text, out resultado))
                 {
-                    DateTime resultado;
-
-                    if (DateTime.TryParse(result.RecognitionResult.Lines[12].Text, out resultado))
-                    {
-                        cadastro.DtNascimento = resultado;
-                    }
+                    cadastro.DtNascimento = resultado;
                 }
-
-                cadastro.NomeMae = result.RecognitionResult.Lines[19].Text;
-
-                {
-                    DateTime resultado;
-
-                    if (DateTime.TryParse(result.RecognitionResult.Lines[29].Text, out resultado))
-                    {
-                        cadastro.Validade = resultado;
-                    }
-                }
-
-                
             }
-            catch (Exception ex)
+
+            cadastro.NomeMae = result.RecognitionResult.Lines[19].Text;
+
             {
-                BadRequest(ex.Message);
+                DateTime resultado;
+
+                if (DateTime.TryParse(result.RecognitionResult.Lines[29].Text, out resultado))
+                {
+                    cadastro.Validade = resultado;
+                }
+
+
             }
 
             return Ok(cadastro);
